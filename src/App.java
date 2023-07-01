7efdca836e1697389733165d35f346d96f920e26
Jose Carmona <manuel_andeliz@hotmail.com>
2023-07-01 11:27:00 -0400

    commit 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+       Scanner firstName = new Scanner(System.in);
+
+        System.out.println("Enter your first name: ");
+        String first = firstName.next();
+
+
+        System.out.println("Enter your last name: ");
+        Scanner lastName = new Scanner(System.in);
+        String last = lastName.next();
+
+        Scanner age = new Scanner(System.in);
+        System.out.println("Enter your age ");
+        int ageNumber = age.nextInt();
+
+        Scanner birthMonth = new Scanner(System.in);
+        System.out.println("Birth month: ");
+        int month = birthMonth.nextInt();
+
+        Scanner numberOfSiblings = new Scanner(System.in);
+        System.out.println("how many siblings you have? ");
+        int siblings = numberOfSiblings.nextInt();
+
+        Scanner favorite
     }
+
+
+
 }
